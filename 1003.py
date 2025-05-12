b3f95f790672d68870d5bf777d39ab59677cf34a
himinqq <alsrud0966@naver.com>
2025-05-12 22:04:05 +0900

    baekjooon: 피보나치 함수

---
+++ 1003.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+# fibo 호출 횟수 저장: dp[i] = [fibo(0) 호출 횟수, fibo(1) 호출 횟수]
+dp = [[0, 0] for _ in range(41)]
+dp[0] = [1, 0]
+dp[1] = [0, 1]
+
+for i in range(2, 41):
+    dp[i][0] = dp[i - 1][0] + dp[i - 2][0]
+    dp[i][1] = dp[i - 1][1] + dp[i - 2][1]
+
+for _ in range(t):
+    n = int(input())
+    print(*dp[n])
