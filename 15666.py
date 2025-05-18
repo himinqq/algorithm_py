17dbc6dac463ee92d2b97709686348e2554f32ca
himinqq <alsrud0966@naver.com>
2025-05-19 00:05:54 +0900

    baekjoon: n과m(12)

--- 15666.py
+++ 15666.py
@@ -1,18 +1,20 @@
-
 n,m = map(int,input().split())
-numbers = list(map(int,input().split()))
+
 arr = [0]*m
+numbers = list(map(int,input().split()))
 numbers.sort()
+
 seen = set()
 
-def backtrack(k):
+def backtrack(start,k):
     if k == m:
         key = tuple(arr)
         if key not in seen:
-            print(*arr)
             seen.add(key)
+            print(*arr)
         return
-    for i in range(n):
+    for i in range(start,n):
         arr[k] = numbers[i]
-        backtrack(k + 1)
-backtrack(0)
+        backtrack(i, k + 1)
+
+backtrack(0,0)
