f1e02c45ef7a572b5d41605e3d5c660ebfc6160c
himinqq <alsrud0966@naver.com>
2025-06-19 23:02:20 +0900

    baekjoon: 최소비용 구하기2

--- 11779.py
+++ 11779.py
@@ -1,18 +1,19 @@
 import heapq
-import sys
 
-input = sys.stdin.readline
+city_n = int(input())
+bus_m = int(input())
 
 INF = int(1e9)
-v,e = map(int,input().split())
-k = int(input())
 
-graph = [[] for _ in range(v+1)]
-dist = [INF]* (v+1)
+graph = [[] for _ in range(city_n+1)]
+dist = [INF] * (city_n+1)
+prev = [0] * (city_n+1)  # 경로 추적용
 
-for _ in range(e):
+for _ in range(bus_m):
     a,b,c = map(int,input().split())
     graph[a].append((b,c))
+
+start, end = map(int,input().split())
 
 def dijkstra(start):
     q = []
@@ -21,20 +22,24 @@ def dijkstra(start):
 
     while q:
         cur_dist, node = heapq.heappop(q)
-
         if dist[node] < cur_dist:
             continue
-
         for adj in graph[node]:
             cost = cur_dist + adj[1]
             if cost < dist[adj[0]]:
                 dist[adj[0]] = cost
+                prev[adj[0]] = node
                 heapq.heappush(q,(cost,adj[0]))
+dijkstra(start)
 
-dijkstra(k)
+path = []
+curr = end
+while curr != 0:
+    path.append(curr)
+    curr = prev[curr]
+path.reverse()
 
-for d in range(1,len(dist)):
-    if dist[d] == INF:
-        print("INF")
-    else:
-        print(dist[d])
+print(dist[end]) #최소비용
+print(len(path)) #도시수
+print(*path) #경로
+
