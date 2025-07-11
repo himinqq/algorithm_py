886165f3e26d77328959412c511d2d84a0fad0a5
himinqq <alsrud0966@naver.com>
2025-07-11 11:30:25 +0900

    baekjoon: 연구소3

--- 17142.py
+++ 17142.py
@@ -1,12 +1,8 @@
 from collections import deque
 from itertools import combinations
 
-n, m = map(int, input().split())
-
+n, virus_num = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
-
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
 
 virus_loc = []
 for i in range(n):
@@ -14,39 +10,38 @@ for i in range(n):
         if board[i][j] == 2:
             virus_loc.append([i, j])
 
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
-def bfs(active):
-    dist = [[-1] * n for _ in range(n)]
+
+def spread_virus(virus):
     q = deque()
-    for x, y in active:
+    dist = [[-1] * n for _ in range(n)]
+    for x, y in virus:
         q.append((x, y))
         dist[x][y] = 0
 
     while q:
-        x, y = q.popleft()
+        cx, cy = q.popleft()
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+            nx, ny = cx + dx[i], cy + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= n:
                 continue
-            if dist[nx][ny] >= 0:
-                continue
-            if board[nx][ny] != 1:
+            if board[nx][ny] != 1 and dist[nx][ny] == -1:
                 q.append((nx, ny))
-                dist[nx][ny] = dist[x][y] + 1
+                dist[nx][ny] = dist[cx][cy] + 1
     max_time = 0
     for i in range(n):
         for j in range(n):
-            # 감염되지 않은 빈 칸 또는 '선택되지 않은' 바이러스 칸2
-            if board[i][j] != 1 and (i, j) not in active:
+            if board[i][j] == 0:
                 if dist[i][j] == -1:
                     return float('inf')
                 max_time = max(max_time, dist[i][j])
     return max_time
 
-
 min_time = float('inf')
-for comb in combinations(virus_loc, m):
-    time = bfs(comb)
-    min_time = min(min_time, time)
+for comb in combinations(virus_loc, virus_num):
+    time = spread_virus(comb)
+    min_time = min(time, min_time)
 
 print(min_time if min_time != float('inf') else -1)
