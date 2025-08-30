c9954dd6c8a4d33aa9698cae7f75b78638229f4b
himinqq <alsrud0966@naver.com>
2025-08-30 19:06:16 +0900

    baekjoon: 최소스패닝트리

---
+++ kruskal.py
@@ -0,0 +1,40 @@
+import sys
+
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+v, e = map(int, input().split())
+
+edges = []
+parent = [0] * (v + 1)
+for i in range(1, v + 1):
+    parent[i] = i
+
+
+def find(parent, x):
+    if parent[x] != x:
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+total_cost = 0
+for i in range(e):
+    cost, a, b = edges[i]
+    if find(parent, a) != find(parent, b):
+        union(parent, a, b)
+        total_cost += cost
+
+print(total_cost)
